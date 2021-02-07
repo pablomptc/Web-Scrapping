0a537d9b72e6d2636989eb13936b7ba91afce111
Pablo <37776542+pablomptc@users.noreply.github.com>
2021-02-07 17:58:34 +0100

    Update and rename Asociación2.py to Association algorithm (market-basket).py

--- Association algorithm (market-basket).py
+++ Association algorithm (market-basket).py
@@ -2,9 +2,9 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 
-df = pd.read_csv ("Asociación.csv")
+df = pd.read_csv ("association_input.csv")
 
 frequent_itemsets = apriori(df > 0, min_support = 0.06, use_colnames = True)
 rules = association_rules (frequent_itemsets, metric = "confidence", min_threshold = 0.8)
 rules = association_rules (frequent_itemsets, metric = "lift", min_threshold = 1)
-rules.to_csv ("asociation.csv")
+rules.to_csv ("association_output.csv")
